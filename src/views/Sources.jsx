02ac5d037761b2f7e3a0d3c1341d0c0ad5b1857f
alejandro <alejandro@alejandros-MacBook-Pro.local>
2021-05-18 18:51:38 -0500

    add sources and welcome text

--- src/views/Sources.jsx
+++ src/views/Sources.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import CardButton from '../components/CardButton';
 import PoliHeader from '../components/Header';
 import PoliFooter from '../components/Footer';
+import itemSources from '../content/sources';
 
 function Sources(){
+    console.log(itemSources);
     function sendTo(page){
         window.location.href = page;
     }//sendTo
@@ -26,6 +28,13 @@ function Sources(){
                                 textColor={"white"}
                             />
                         </div>  
+                    </Col>
+                </Row>
+                <Row className="justify-content-center m-v-space">
+                    <Col sm={10}>
+                        <div className="full-card">
+                            {itemSources.map(item => <p>{item}</p>)}
+                        </div>
                     </Col>
                 </Row>
             </Container>
