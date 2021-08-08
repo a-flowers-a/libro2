52f73911499cf1341a01a7a870a11b480cd2fb70
Brees-T <37353793+Brees-T@users.noreply.github.com>
2021-08-08 02:36:24 -0500

    Removed book view and redirect to first unit on the book

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Welcome from './views/Welcome';
-import Book from './views/Book';
+import Welcome from "./views/Welcome";
 import Sources from "./views/Sources";
 import Method from "./views/Method";
 import TwoOne from "./views/UnidadII/TwoOne";
@@ -12,17 +11,24 @@ import TwoThreeTwo from "./views/UnidadII/TwoThreeTwo";
 function App() {
   return (
     <Router>
-        <Switch>
-            <Route path="/" exact component={() => <Welcome />} />
-            <Route path="/book" exact component={() => <Book />} />
-            <Route path="/sources" exact component={() => <Sources />} />
-            <Route path="/methodology" exact component={() => <Method />} />
-            <Route path="/content/II/2.1" exact component={() => <TwoOne /> } />
-            <Route path="/content/II/2.2" exact component={() => <TwoTwo /> } />
-            <Route path="/content/II/2.3" exact component={() => <TwoThree /> } />
-            <Route path="/content/II/2.3.1" exact component={() => <TwoThreeOne /> } />
-            <Route path="/content/II/2.3.2" exact component={() => <TwoThreeTwo /> } />
-        </Switch>
+      <Switch>
+        <Route path="/" exact component={() => <Welcome />} />
+        <Route path="/sources" exact component={() => <Sources />} />
+        <Route path="/methodology" exact component={() => <Method />} />
+        <Route path="/content/II/2.1" exact component={() => <TwoOne />} />
+        <Route path="/content/II/2.2" exact component={() => <TwoTwo />} />
+        <Route path="/content/II/2.3" exact component={() => <TwoThree />} />
+        <Route
+          path="/content/II/2.3.1"
+          exact
+          component={() => <TwoThreeOne />}
+        />
+        <Route
+          path="/content/II/2.3.2"
+          exact
+          component={() => <TwoThreeTwo />}
+        />
+      </Switch>
     </Router>
   );
 }
