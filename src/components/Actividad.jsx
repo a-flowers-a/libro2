0281af3480fc59cbd5d98c37a4e80522134fd0bd
Alejandro Flowers <a_flowers_a@outlook.com>
2021-09-25 19:14:22 -0500

    center elements in button component

--- src/components/Actividad.jsx
+++ src/components/Actividad.jsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Actividad(props){
-    const {link, number, type, icon} = props;
-    function download(){
-        window.open(link);
-    }
-    return(
-        <div className="card-button purple-bg white"
-            onClick={download}
-        >
-            <FontAwesomeIcon 
-                className={'icon'}
-                icon={icon} 
-                size="2x"
-            />
-            {`${type} ${number}`}
-        </div>
-    );
-}//Actividad
+function Actividad(props) {
+  const { link, number, type, icon } = props;
+  function download() {
+    window.open(link);
+  }
+  return (
+    <div className="card-button purple-bg white" onClick={download}>
+      <FontAwesomeIcon className={"icon"} icon={icon} size="2x" />
+      <div>{`${type} ${number}`}</div>
+    </div>
+  );
+} //Actividad
 
 export default Actividad;
