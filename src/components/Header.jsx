9a535f275b9c97e3694fb5af04f9bf57441967ed
Alejandro Flowers <a_flowers_a@outlook.com>
2021-09-26 16:51:38 -0500

    book's title in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import Image from 'react-bootstrap/Image';
+import React from "react";
+import { Row, Col, Container, Table, Image } from "react-bootstrap";
 
 function Header() {
-    const ipn = "../../assets/images/Logo_IPN.png";
-    const escom = "../../assets/images/Logo_ESCOM.png";
-    return (
-        <div className="header">
-            <Image className="responsiveLogo set-left" src={ipn} alt="Logo IPN" />
-            <Image className="responsiveLogo set-right" src={escom} alt="Logo ESCOM" />
-        </div>
-    );
-}//Header
+  const ipn = "../../assets/images/Logo_IPN.png";
+  const escom = "../../assets/images/Logo_ESCOM.png";
+  return (
+    <Container fluid className="header">
+      <Row>
+        <Col sm={3}>
+          <Image className="responsiveLogo set-left" src={ipn} alt="Logo IPN" />
+        </Col>
+        <Col className="header-title-container">
+          <div>Polilibro de Finanzas Empresariales</div>
+        </Col>
+        <Col sm={3}>
+          <Image
+            className="responsiveLogo set-right"
+            src={escom}
+            alt="Logo ESCOM"
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+} //Header
 
 export default Header;
