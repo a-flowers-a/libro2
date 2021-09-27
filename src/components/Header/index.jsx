c86d7a82741713768801c78806c8f9ec578bca2d
Alejandro Flowers <a_flowers_a@outlook.com>
2021-09-26 19:07:58 -0500

    move header styles into header folder, modify header to add book title and position of logos

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Row, Col, Container, Image } from "react-bootstrap";
+//style
+import "./style.css";
+
+function Header() {
+  const ipn = "../../assets/images/Logo_IPN.png";
+  const escom = "../../assets/images/Logo_ESCOM.png";
+  return (
+    <Container fluid className="header">
+      <Row>
+        <Col
+          xs={{ span: 3, order: 1 }}
+          className="center-flex-content image-container"
+        >
+          <Image className="header-image" src={ipn} alt="Logo IPN" />
+        </Col>
+        <Col xs={{ span: 3, order: 3 }} className="center-flex-content">
+          <Image className="header-image" src={escom} alt="Logo ESCOM" />
+        </Col>
+        <Col
+          xs={{ span: 6, order: 2 }}
+          className="center-flex-content header-title-container"
+        >
+          <div>Polilibro de Finanzas Empresariales</div>
+        </Col>
+      </Row>
+    </Container>
+  );
+} //Header
+
+export default Header;
