487c42f4341c8084ca96657347c9d0fc68112d3c
Brees-T <37353793+Brees-T@users.noreply.github.com>
2021-10-11 23:29:02 -0500

    added unit 3 topics

---
+++ src/constants/unitThree.js
@@ -0,0 +1,86 @@
+export const unitThree = {
+  name: "unitOne",
+  content: [
+    { name: "Introducción", path: "/content/III/intro", level: 1 },
+    {
+      name: "3.1 El análisis financiero en la empresa",
+      path: "/content/III/3.1",
+      level: 1,
+    },
+    {
+      name: "3.2 Métodos para el análisis financiero",
+      path: "/content/III/3.2",
+      level: 1,
+    },
+    {
+      name: "3.2.1 Análisis vertical (razones financieras y Dupont)",
+      path: "/content/III/3.2.1",
+      level: 2,
+    },
+    {
+      name: "3.2.1.1 Método de razones simples",
+      path: "/content/III/3.2.1.1",
+      level: 3,
+    },
+    { name: "3.2.1.2 Método Dupont", path: "/content/III/3.2.1.2", level: 3 },
+    {
+      name: "3.2.1.3 Método de razones estándar",
+      path: "/content/III/3.2.1.3",
+      level: 3,
+    },
+    { name: "3.2.2 Análisis horizontal", path: "/content/III/3.2.2", level: 2 },
+    {
+      name: "3.2.2.1 Método de aumentos y disminuciones, también llamado método de diferencias",
+      path: "/content/III/3.2.2.1",
+      level: 3,
+    },
+    {
+      name: "3.2.2.2 Método de tendencias",
+      path: "/content/III/3.2.2.2",
+      level: 3,
+    },
+    {
+      name: "3.2.2.3 Método de control presupuestal",
+      path: "/content/III/3.2.2.3",
+      level: 3,
+    },
+    {
+      name: "3.3 Análisis del punto de equilibrio operacional",
+      path: "/content/III/3.3",
+      level: 1,
+    },
+    {
+      name: "3.3.1 Modelo matemático del punto de equilibrio con un producto",
+      path: "/content/III/3.3.1",
+      level: 2,
+    },
+    {
+      name: "3.3.1.1 Punto de equilibrio con un producto sin utilidad",
+      path: "/content/III/3.3.1.1",
+      level: 3,
+    },
+    {
+      name: "3.3.1.2 Punto de equilibrio con un producto con utilidad",
+      path: "/content/III/3.3.1.2",
+      level: 3,
+    },
+    {
+      name: "3.3.2 Modelo matemático del punto de equilibrio con multiproductos (mezcla de dos o más productos)",
+      path: "/content/III/3.3.2",
+      level: 2,
+    },
+    {
+      name: "3.3.2.1 punto de equilibrio para una mezcla de productos sin utilidad",
+      path: "/content/III/3.3.2.1",
+      level: 3,
+    },
+    {
+      name: "3.3.2.2 punto de equilibrio para una mezcla de productos con utilidad",
+      path: "/content/III/3.3.2.2",
+      level: 3,
+    },
+    { name: "Cierre", path: "/content/III/cierre", level: 1 },
+  ],
+};
+
+export default unitThree;
